de70a33e87c49fb1db5ac128da00b4a5f96aa48b
swallowtail1056-collab <swallowtail1056@gmail.com>
2026-08-10 11:27:45 +0900

    Add files via upload

---
+++ match-sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'kitsunechess-match-v1';
+const CORE_ASSETS = [
+  '/match.html',
+  '/match_icon_192.png',
+  '/match_icon_512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
